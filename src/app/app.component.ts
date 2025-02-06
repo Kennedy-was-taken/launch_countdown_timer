688900f8860e952f6fe4d0d669d894143b94b98a
Kennedy <tuks1032@gmail.com>
2025-02-07 00:52:29 +0200

    desktop, tablet, mobile design completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css', './styles/landscape/mobileMedia.css', './styles/portrait/tabletMedia.css', './styles/landscape/tabletMedia.css', './styles/landscape/desktopMedia.css']
 })
 export class AppComponent {
   title = 'launch_countdown_timer';
